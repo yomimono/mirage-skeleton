46dc8ac6f064cc3b2a0bd52b2e0b1252f63b511b
Mindy Preston <mindy.preston@docker.com>
2016-11-11 16:17:15 -0600

    replace console with logs in network example

--- network/config.ml
+++ network/config.ml
@@ -1,10 +1,10 @@
 open Mirage
 
-let main = foreign "Unikernel.Main" (console @-> stackv4 @-> job)
+let main = foreign "Unikernel.Main" (stackv4 @-> job)
 
 let stack = generic_stackv4 tap0
 
 let () =
   register "network" [
-    main $ default_console $ stack
+    main $ stack
   ]
--- network/unikernel.ml
+++ network/unikernel.ml
@@ -1,31 +1,24 @@
 open Lwt.Infix
 
-let red fmt    = Printf.sprintf ("\027[31m"^^fmt^^"\027[m")
-let green fmt  = Printf.sprintf ("\027[32m"^^fmt^^"\027[m")
-let yellow fmt = Printf.sprintf ("\027[33m"^^fmt^^"\027[m")
-let blue fmt   = Printf.sprintf ("\027[36m"^^fmt^^"\027[m")
+module Main (S: V1_LWT.STACKV4) = struct
 
-module Main (C: V1_LWT.CONSOLE) (S: V1_LWT.STACKV4) = struct
+  let src = Logs.Src.create "network" ~doc:"Example hetwork stack user"
+  module Log = (val Logs.src_log src: Logs.LOG)
 
-  let start c s =
+  let start s =
     S.listen_tcpv4 s ~port:8080 (fun flow ->
         let dst, dst_port = S.TCPV4.dst flow in
-        C.log_s c (green "new tcp connection from %s %d"
-                     (Ipaddr.V4.to_string dst) dst_port)
-        >>= fun () ->
+        Log.info (fun f -> f "new tcp connection from %s %d"
+                     (Ipaddr.V4.to_string dst) dst_port);
         S.TCPV4.read flow
         >>= function
         | `Ok b ->
-          C.log_s c
-            (yellow "read: %d\n%s"
-               (Cstruct.len b) (Cstruct.to_string b)
-            )
-          >>= fun () ->
+          Log.info (fun f -> f
+            "read: %d\n%s" (Cstruct.len b) (Cstruct.to_string b)
+          );
           S.TCPV4.close flow
-
-        | `Eof -> C.log_s c (red "read: eof")
-
-        | `Error _e -> C.log_s c (red "read: error")
+        | `Eof -> Log.info (fun f -> f "read: eof"); Lwt.return_unit
+        | `Error _e -> Log.info (fun f -> f "read: error"); Lwt.return_unit
       );
 
     S.listen s
