0a65367cea9da21ab80e59fb7881442d364c6b4e
Mindy Preston <meetup@yomimono.org>
2017-08-04 17:57:05 -0500

    depend on mirage-dns, not dns.mirage, in dns example

--- applications/dns/config.ml
+++ applications/dns/config.ml
@@ -3,11 +3,11 @@ open Mirage
 (** Always compile DNS zone file data into the unikernel *)
 let data = crunch "./data"
 
-(** Define the shape of the service.  We depend on the `dns`
+(** Define the shape of the service.  We depend on the `mirage-dns`
     package, and it requires a logging console, a read-only
     key/value store and a TCP/IP stack. *)
 let dns_handler =
-  let packages = [package ~min:"0.20.0" ~sublibs:["mirage"] "dns"; package "duration"] in
+  let packages = [package ~min:"3.0.0" "mirage-dns"; package "duration"] in
   foreign
     ~packages
     "Unikernel.Main" (kv_ro @-> stackv4 @-> job)
